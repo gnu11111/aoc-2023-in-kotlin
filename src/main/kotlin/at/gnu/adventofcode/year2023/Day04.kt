e136c672a03266993e3bf1daf28b3e6632eea105
gnu11111 <gnu@gnu.at>
2023-12-05 02:01:34 +0100

    polishing

--- src/main/kotlin/at/gnu/adventofcode/year2023/Day04.kt
+++ src/main/kotlin/at/gnu/adventofcode/year2023/Day04.kt
@@ -9,33 +9,31 @@ class Day04(input: List<String>) {
         val WHITESPACES = """\s+""".toRegex()
     }
 
+    data class Card(val winningNumbers: Set<Int>, val myNumbers: Set<Int>)
+
     private val cards = input.mapNotNull { line ->
         if (line.isBlank())
             null
         else {
-            val parts = line.split(":")
-            val cardNumber = parts[0].trim().split(WHITESPACES)[1].trim().toInt()
-            val allNumbers = parts[1].split("|")
+            val allNumbers = line.split(":")[1].split("|")
             val winningNumbers = allNumbers[0].trim().split(WHITESPACES).map(String::toInt).toSet()
             val myNumbers = allNumbers[1].trim().split(WHITESPACES).map(String::toInt).toSet()
-            Triple(cardNumber, winningNumbers, myNumbers)
+            Card(winningNumbers, myNumbers)
         }
     }
 
     fun part1(): Int =
         cards.fold(0) { acc, card ->
-            val winning = (card.second intersect card.third).size
+            val winning = (card.winningNumbers intersect card.myNumbers).size
             if (winning > 0) acc + 2.0.pow(winning - 1.0).toInt() else acc
         }
 
     fun part2(): Int {
-        val winningCards = Array(cards.maxOf { it.first } + 1) { 0 }
-        for ((cardNumber, winningNumbers, myNumbers) in cards) {
-            val winning = (winningNumbers intersect myNumbers).size
+        val winningCards = Array(cards.size) { 0 }
+        for ((cardNumber, numbers) in cards.withIndex()) {
             winningCards[cardNumber]++
-            if (winning > 0)
-                ((cardNumber + 1)..(cardNumber + winning)).forEach { winningCards[it] += winningCards[cardNumber] }
-//            println("$cardNumber: ${winningCards.toList().drop(1)}")
+            val winning = (numbers.winningNumbers intersect numbers.myNumbers).size
+            (1..winning).forEach { winningCards[cardNumber + it] += winningCards[cardNumber] }
         }
         return winningCards.sum()
     }
