f2b94ae66004ea5e5dd52c0db787646700c5202a
gnu11111 <gnu@gnu.at>
2023-12-11 23:21:12 +0100

    Day 11 - Cosmic Expansion

---
+++ src/main/kotlin/at/gnu/adventofcode/year2023/Day11.kt
@@ -0,0 +1,62 @@
+package at.gnu.adventofcode.year2023
+
+import kotlin.math.abs
+
+class Day11(image: List<String>) {
+
+    companion object {
+        const val RESOURCE = "/adventofcode/year2023/Day11.txt"
+    }
+
+    data class Galaxy(val x: Int, val y: Int)
+
+    private val universe = image.mapIndexed { y, row ->
+        row.mapIndexedNotNull { x, c ->
+            if (c == '#') Galaxy(x, y) else null
+        }
+    }.flatten()
+
+    private val emptyRows = image.mapIndexedNotNull { y, row -> if (row == ".".repeat(row.length)) y else null }.toSet()
+    private val emptyCols: Set<Int>
+
+    init {
+        val temp = mutableSetOf<Int>()
+        for (x in image.first().indices) {
+            var empty = true
+            for (row in image) {
+                if (row[x] == '#') {
+                    empty = false
+                    break
+                }
+            }
+            if (empty) temp += x
+        }
+        emptyCols = temp.toSet()
+    }
+
+
+    fun part1(size: Int): Long =
+        universe.expand(size).calculateTotalDistance()
+
+    fun part2(size: Int): Long =
+        universe.expand(size).calculateTotalDistance()
+
+    private fun List<Galaxy>.expand(size: Int): List<Galaxy> =
+        map { galaxy ->
+            val emptyColsBefore = emptyCols.count { it < galaxy.x }
+            val emptyRowsBefore = emptyRows.count { it < galaxy.y }
+            Galaxy(galaxy.x + (emptyColsBefore * (size - 1)), galaxy.y + (emptyRowsBefore * (size - 1)))
+        }
+
+    private fun List<Galaxy>.calculateTotalDistance(): Long =
+        indices.sumOf { i -> ((i + 1) until size).sumOf { j -> calculateDistance(this[i], this[j]).toLong() } }
+
+    private fun calculateDistance(galaxy1: Galaxy, galaxy2: Galaxy): Int =
+        abs(galaxy1.x - galaxy2.x) + abs(galaxy1.y - galaxy2.y)
+}
+
+fun main() {
+    val day11 = Day11(Day11::class.java.getResource(Day11.RESOURCE)!!.readText().trim().split("\n", "\r\n"))
+    println("Day11::part1 -> ${day11.part1(2)}")
+    println("Day11::part2 -> ${day11.part2(1_000_000)}")
+}
---
+++ src/test/kotlin/at/gnu/adventofcode/year2023/Day11Test.kt
@@ -0,0 +1,28 @@
+package at.gnu.adventofcode.year2023
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
+
+class Day11Test {
+
+    private val image = listOf("...#......", ".......#..", "#.........", "..........", "......#...", ".#........",
+        ".........#", "..........", ".......#..", "#...#.....")
+
+    private val test = mapOf(
+        Day11::part1 to 2 to 374L,
+        Day11::part2 to 10 to 1030L,
+        Day11::part2 to 100 to 8410L
+    )
+
+    @Test @ExperimentalTime
+    fun testMySolution() {
+        val day11 = Day11(image)
+        for ((function, size) in test.keys) {
+            val (result, time) = measureTimedValue { function.invoke(day11, size) }
+            println("Day11::${function.name}: ${image.size} rows -> $result [${time}]")
+            assertEquals(test[function to size], result)
+        }
+    }
+}
